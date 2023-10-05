af98764ccd7abc5b5f3b8d49ba3bb140bfe7257b
Ankit Jha (BlackBeltHelp) <ankit_jha@blackbelthelp.com>
2023-10-05 15:43:04 +0530

    first commit

---
+++ Lecture1.js
@@ -0,0 +1,17 @@
+
+        // console.log("Namaste duniya");
+
+// let names = ["love","babbar","ankit","jha"]
+// names[1] = "123"
+// console.log(names )
+
+// let a = "string"
+// a = 34
+// console.log(a)
+
+// let age = 19
+
+// let status = (age>=18) ? "I can vote" : "I cannot vote"
+// console.log(status)
+
+true || false
